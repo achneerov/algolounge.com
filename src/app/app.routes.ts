95d545ba4f90da0ce4d12fb1b2cd724debd67dd7
Alexander Chneerov <achneerov@gmail.com>
2025-06-14 13:00:02 -0400

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { QuestionComponent } from "./component/question/question.component";
+import { NotFoundComponent } from "./component/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "question", component: QuestionComponent },
+  { path: "**", component: NotFoundComponent },
+];

c25b9e969ece1dd2d9fab9c6c253d8c9b85ffeb3
Alexander Chneerov <achneerov@gmail.com>
2025-06-15 17:20:27 -0400

    s

--- src/app/component/question/question.component.ts
+++ src/app/component/question/question.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { IdeComponent } from "../ide/ide.component";
 import { QuestionHeaderComponent } from "../question-header/question-header.component";
+import { ConsoleComponent } from "../console/console.component";
 
 @Component({
   selector: "app-question",
-  imports: [IdeComponent, QuestionHeaderComponent],
+  imports: [IdeComponent, QuestionHeaderComponent, ConsoleComponent],
   templateUrl: "./question.component.html",
   styleUrl: "./question.component.scss",
 })

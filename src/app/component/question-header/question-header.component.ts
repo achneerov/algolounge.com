f1c7b2067a9953fc9449a94b676306e3e6f85d3b
Alexander Chneerov <achneerov@gmail.com>
2025-06-15 17:18:07 -0400

    s

---
+++ src/app/component/question-header/question-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-question-header",
+  imports: [],
+  templateUrl: "./question-header.component.html",
+  styleUrl: "./question-header.component.scss",
+})
+export class QuestionHeaderComponent {
+  onRun(): void {}
+}
--- src/app/component/question/question.component.ts
+++ src/app/component/question/question.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { IdeComponent } from "../ide/ide.component";
+import { QuestionHeaderComponent } from "../question-header/question-header.component";
 
 @Component({
   selector: "app-question",
-  imports: [IdeComponent],
+  imports: [IdeComponent, QuestionHeaderComponent],
   templateUrl: "./question.component.html",
   styleUrl: "./question.component.scss",
 })

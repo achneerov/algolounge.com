54348ee07f80309a4f877080009e730d10875df6
Alexander Chneerov <achneerov@gmail.com>
2025-06-15 16:58:17 -0400

    s

--- src/app/component/ide/ide.component.ts
+++ src/app/component/ide/ide.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { EditorView, basicSetup } from "codemirror";
+import { python } from "@codemirror/lang-python";
 
 @Component({
-  selector: 'app-ide',
+  selector: "app-ide",
   imports: [],
-  templateUrl: './ide.component.html',
-  styleUrl: './ide.component.scss'
+  templateUrl: "./ide.component.html",
+  styleUrl: "./ide.component.scss",
 })
-export class IdeComponent {
+export class IdeComponent implements OnInit {
+  @ViewChild("editor", { static: true }) editorElement!: ElementRef;
 
+  ngOnInit() {
+    new EditorView({
+      parent: this.editorElement.nativeElement,
+      doc: 'print("Hello world")',
+      extensions: [basicSetup, python()],
+    });
+  }
 }

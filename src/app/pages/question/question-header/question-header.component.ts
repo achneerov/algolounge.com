813614738e61ebe0869a7731d36df17d99e2d4a8
Alexander Chneerov <achneerov@gmail.com>
2025-06-21 12:18:30 -0400

    s

--- src/app/pages/question/question-header/question-header.component.ts
+++ src/app/pages/question/question-header/question-header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-question-header",
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: "./question-header.component.html",
   styleUrl: "./question-header.component.scss",
 })

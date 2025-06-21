f5ff0619ca9e30c2df724149c9a9557195dd9d53
Alexander Chneerov <achneerov@gmail.com>
2025-06-21 12:44:47 -0400

    s

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -2,10 +2,18 @@ import { Component } from "@angular/core";
 import { IdeComponent } from "./ide/ide.component";
 import { QuestionHeaderComponent } from "./question-header/question-header.component";
 import { ConsoleComponent } from "./console/console.component";
+import { DescriptionComponent } from "./description/description.component";
+import { SplitterModule } from "primeng/splitter";
 
 @Component({
   selector: "app-question",
-  imports: [IdeComponent, QuestionHeaderComponent, ConsoleComponent],
+  imports: [
+    IdeComponent,
+    QuestionHeaderComponent,
+    ConsoleComponent,
+    DescriptionComponent,
+    SplitterModule,
+  ],
   templateUrl: "./question.component.html",
   styleUrl: "./question.component.scss",
 })

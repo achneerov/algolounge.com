69cd109330db0910479516d939c63184afe91da6
Alexander Chneerov <achneerov@gmail.com>
2025-06-21 11:53:12 -0400

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from "@angular/router";
-import { QuestionComponent } from "./page/question/question.component";
-import { NotFoundComponent } from "./page/not-found/not-found.component";
+import { QuestionComponent } from "./pages/question/question.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   { path: "question", component: QuestionComponent },
+  { path: "home", component: HomeComponent },
+
   { path: "**", component: NotFoundComponent },
 ];

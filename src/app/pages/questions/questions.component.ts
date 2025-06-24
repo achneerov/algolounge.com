22962b51a8261c6302a2ac35c8894d2e168fd024
Alexander Chneerov <achneerov@gmail.com>
2025-06-24 13:01:14 -0400

    s

--- src/app/pages/questions/questions.component.ts
+++ src/app/pages/questions/questions.component.ts
@@ -7,6 +7,7 @@ import { SplitterModule } from "primeng/splitter";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { NotFoundComponent } from "../not-found/not-found.component";
 
 @Component({
   selector: "app-questions",
@@ -19,6 +20,7 @@ import { HttpClient, HttpClientModule } from "@angular/common/http";
     ConsoleComponent,
     DescriptionComponent,
     SplitterModule,
+    NotFoundComponent,
   ],
   templateUrl: "./questions.component.html",
   styleUrl: "./questions.component.scss",
@@ -45,13 +47,17 @@ export class QuestionsComponent {
     this.router.navigate(["/questions", id]);
   }
 
+  notFound = false;
+
   loadQuestion(id: string) {
+    this.notFound = false;
     this.http.get<{ description: string }>(`/questions/${id}.json`).subscribe({
       next: (data) => {
         this.description = data.description;
+        this.notFound = false;
       },
       error: () => {
-        this.router.navigate(["/not-found"]);
+        this.notFound = true;
       },
     });
   }

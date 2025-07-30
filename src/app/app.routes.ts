2bf326c691f5e3193c334017091dd3f6178a8f67
Alexander Chneerov <achneerov@gmail.com>
2025-07-29 21:37:01 -0400

    added home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NotFoundComponent } from "./pages/not-found/not-found.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "questions/:id", component: QuestionsComponent },
   { path: "questions", redirectTo: "questions/0", pathMatch: "full" },
   { path: "home", component: HomeComponent },

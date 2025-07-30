0d8c17f95042c1acd92d52b82f0abd030b408f2b
Alexander Chneerov <achneerov@gmail.com>
2025-07-29 22:45:01 -0400

    fix: correct import case for Aura theme in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { providePrimeNG } from "primeng/config";
-import Aura from "@primeng/themes/Aura";
+import Aura from "@primeng/themes/aura";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 

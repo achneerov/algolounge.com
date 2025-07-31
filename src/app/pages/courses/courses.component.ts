f83bf8d7efb9256fd71fb5cd2f5d36b5e2dcc315
Alexander Chneerov <achneerov@gmail.com>
2025-07-31 19:50:25 -0400

    fixed search in courses

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -2,21 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AutoCompleteModule } from 'primeng/autocomplete';
 import { Router } from '@angular/router';
 import { CourseSearchService, CourseSearchResult } from '../../services/course-search.service';
 
 @Component({
   selector: 'app-courses',
-  imports: [CommonModule, FormsModule, HttpClientModule, AutoCompleteModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
-  selectedCourse: any = null;
-  searchResults: CourseSearchResult[] = [];
+  searchTerm = '';
   displayResults: CourseSearchResult[] = [];
-  lastQuery = '';
 
   constructor(private courseSearchService: CourseSearchService, private router: Router) {}
 
@@ -24,28 +21,17 @@ export class CoursesComponent implements OnInit {
     this.courseSearchService.isIndexLoaded().subscribe(loaded => {
       if (loaded) {
         this.displayResults = this.courseSearchService.getAllCourses();
-        this.searchResults = this.displayResults;
       }
     });
   }
 
-  onSearch(event: any) {
-    const query = event.query || '';
-    this.searchResults = this.courseSearchService.searchCourses(query);
-    this.displayResults = this.searchResults;
-    this.lastQuery = query;
+  onSearchChange(event: any) {
+    const query = event.target.value;
+    this.searchTerm = query;
+    this.displayResults = this.courseSearchService.searchCourses(query);
   }
 
   onSelect(course: CourseSearchResult) {
     this.router.navigate(['/courses', course.filename]);
-  }
-
-  onKeyDown(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      if (this.searchResults.length > 0) {
-        this.onSelect(this.searchResults[0]);
-      }
-    }
   }
 }
